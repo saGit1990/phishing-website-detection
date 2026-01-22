d02eaea60911a2ae12dc27ff85d0644464723860
suel ahmed <suel@suels-MacBook-Air.local>
2026-01-23 00:47:41 +0530

    pipeline module completed

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,4 +1,4 @@
-from src.pipeline import run_pipline
+from src.pipeline import run_pipeline
 
 if __name__ == "__main__":
-    run_pipline() 
+    run_pipeline() 
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,32 @@
 from src.config_loader import load_config
-from src.dataloader import load_dataset
-from src.preprocess import preprocess_data
+from src.data_loader import load_dataset
+from src.preprocessor import preprocess_data
 from src.train_xgboost import train_xgboost
 from src.train_ann import train_ann
+
+def run_pipeline():
+    config_loader = load_config()
+
+    df = load_dataset(config_loader['data']['file_path'])
+
+    x_train, x_test, y_train, y_test = preprocess_data(
+        df=df,
+        target_col = config_loader['data']['target_column'],
+        test_size = config_loader['data']['test_size'],
+        random_state = config_loader['data']['random_state'],
+        scaler_path = config_loader['artifacts']['directory']/{config_loader['artifacts']['scaler_filename']}
+    )
+
+    train_xgboost(
+        x_train, x_test, y_train, y_test,
+        params=config_loader['xgboost'],
+        save_path=f"{config_loader['artifacts']['directory']}/{config_loader['artifacts']['xgb_model_filename']}"
+    )
+
+    train_ann(
+        x_train, x_test, y_train, y_test,
+        params=config_loader['mlp'],
+        save_path=f"{config_loader['artifacts']['directory']}/{config_loader['artifacts']['ann_model_filename']}"
+    )
+
+    print('\n Pipeline Completed Sucessfully!')
